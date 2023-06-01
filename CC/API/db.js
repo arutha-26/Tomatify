2024d72d6cfb9237e3292ecc88f4fc115c9e9d45
bharatayasa <100466711+bharatayasa@users.noreply.github.com>
2023-06-01 23:48:14 +0800

    yg bener

---
+++ CC/API/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  // TODO ubah, sesuaikan dengan database di cloud anda 
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'capstone'
+});
+
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Connected to the MySQL server');
+});
+
+module.exports = connection;
