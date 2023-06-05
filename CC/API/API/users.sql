1c8b1c6f36a153ae126954794d0b7bf2b561bd32
Wayan_Bharata <bharatayasa40@gmail.com>
2023-06-05 20:43:51 +0800

    perubahan di database

--- CC/API/API/users.sql
+++ CC/API/API/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 20 Bulan Mei 2023 pada 14.22
+-- Waktu pembuatan: 05 Jun 2023 pada 14.42
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.2.0
 
@@ -37,15 +37,6 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `verification_token`, `is_verified`) VALUES
-(13, 'John Doe', 'johndoe@example.com', 'password123', '93849a09ca5accd83567ed94084ffae644eb7771', 0),
-(16, 'bharata', 'bharatayasa40@gmail.com', '12345678', '0ab931256337bb9b59157503eddbe743c2e57846', 1),
-(21, 'email bangkit', 'c028dsx0642@bangkit.academy', '12345678', '9472ba74c7305aa998be56e8df45f49cd8eb9b7e', 1);
-
---
 -- Indexes for dumped tables
 --
 
@@ -63,7 +54,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
