3e4d3233a3e537bcd2d9977b0da47256a8668aca
Wayan_Bharata <bharatayasa40@gmail.com>
2023-06-10 13:15:39 +0800

    modifikasi database

--- CC/API/API/users.sql
+++ CC/API/API/users.sql
@@ -40,11 +40,6 @@ CREATE TABLE `users` (
 -- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `verification_token`, `is_verified`) VALUES
-(13, 'John Doe', 'johndoe@example.com', 'password123', '93849a09ca5accd83567ed94084ffae644eb7771', 0),
-(16, 'bharata', 'bharatayasa40@gmail.com', '12345678', '0ab931256337bb9b59157503eddbe743c2e57846', 1),
-(21, 'email bangkit', 'c028dsx0642@bangkit.academy', '12345678', '9472ba74c7305aa998be56e8df45f49cd8eb9b7e', 1);
-
 --
 -- Indexes for dumped tables
 --
