c77045c8e4cc0b39edc30681352fdc1b7be9eb88
Wayan_Bharata <bharatayasa40@gmail.com>
2023-06-10 12:52:58 +0800

    ubah di main.py

--- CC/API/API-model/main.py
+++ CC/API/API-model/main.py
@@ -47,7 +47,4 @@ def predict(request):
     predicted_class = class_names[np.argmax(predictions[0])]
     confidence = round(100 * np.max(predictions[0]), 2)
 
-    return {
-              "class": predicted_class, 
-              "confidence": confidence
-            }
+    return { "class": predicted_class, "confidence": confidence}
